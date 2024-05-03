261142849c3bb76e455521372ab21ca2fc5e1ddf
Michael Haynes <michael.haynes@kellogg.northwestern.edu>
2024-05-03 16:04:43 +0000

    break 3

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # get("/things", :controller => "things", :action => "index")
   get("/tacos", :controller => "tacos", :action => "index")
   get("/dice", :controller => "dice", :action => "index")
-  
+  # resources "companies"
+  get("/companies", :controller => "companies", :action => "index")
 end
